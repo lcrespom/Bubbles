b513ddb29dd96020bdd3cc488e4cece499c77723
Luis Crespo <lcrespom@gmail.com>
2017-02-12 09:50:08 +0100

    Tuning animation

--- src/simple-anim.ts
+++ src/simple-anim.ts
@@ -15,7 +15,6 @@ function rnd(range) {
 }
 
 export class Circle extends Figure implements Animation {
-	steps = 0;
 	r: number; g: number; b: number; a: number;
 	dx: number; dy: number;
 
@@ -30,14 +29,15 @@ export class Circle extends Figure implements Animation {
 		this.g = rnd(256);
 		this.b = rnd(256);
 		this.a = 1;
-		this.dx = Math.random() * 4 - 2;
-		this.dy = Math.random() * 4 - 2;
+		this.dx = Math.random() * 2 - 1;
+		this.dy = Math.random() * 2 - 1;
 		this.done = false;
 	}
 
 	step() {
+		this.x += this.dx;
+		this.y += this.dy;
 		this.w += this.a;
-		this.steps++;
 		this.a -= 0.0025;
 		if (this.a <= 0) this.done = true;
 	}
@@ -46,10 +46,14 @@ export class Circle extends Figure implements Animation {
 		gc.strokeStyle = this.strokeStyle;
 		gc.fillStyle = this.fillStyle;
 		this.fillStyle = `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`;
-		this.x += this.dx;
-		this.y += this.dy;
+		this.drawCircle(gc, this.x, this.y);
+		// this.drawCircle(gc, this.x + 3, this.y);
+		// this.drawCircle(gc, this.x, this.y + 3);
+	}
+
+	drawCircle(gc: CanvasRenderingContext2D, x: number, y: number) {
 		gc.beginPath();
-		gc.arc(this.x, this.y, this.w, 0, Math.PI * 2);
+		gc.arc(x, y, this.w, 0, Math.PI * 2);
 		gc.fill();
 	}
 }
